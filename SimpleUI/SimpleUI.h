a97d4d72240275dd68ae213855b6de5906ce2d98
Ingryd <ingrydcduarte@gmail.com>
2025-02-28 11:24:55 -0300

    setup: initialize

---
+++ SimpleUI/SimpleUI.h
@@ -0,0 +1,12 @@
+//
+//  SimpleUI.h
+//  SimpleUI
+//
+//  Created by Ingryd Cordeiro Duarte on 28/02/25.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SimpleUI : NSObject
+
+@end
---
+++ SimpleUI/SimpleUI.m
@@ -0,0 +1,12 @@
+//
+//  SimpleUI.m
+//  SimpleUI
+//
+//  Created by Ingryd Cordeiro Duarte on 28/02/25.
+//
+
+#import "SimpleUI.h"
+
+@implementation SimpleUI
+
+@end
