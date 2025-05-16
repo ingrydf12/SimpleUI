cc2874a08509a56d166096d6d97669f6875732a9
Ingryd <ingrydcduarte@gmail.com>
2025-05-16 11:33:24 -0300

    initial: outline text field

--- SimpleUI/Components/OutlineTextField.swift
+++ SimpleUI/Components/OutlineTextField.swift
@@ -7,12 +7,20 @@
 
 import SwiftUI
 
-struct OutlineTextFiled: View {
+struct OutlineTextField: View {
+    let placeholder: String = "Busca"
+    @State var userSearch: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TextField(placeholder, text: $userSearch)
+            .padding()
+            .frame(maxWidth: .infinity, minHeight: 50)
+            .disableAutocorrection(true)
+            .textFieldStyle(.roundedBorder)
     }
 }
 
 #Preview {
-    OutlineTextFiled()
+    OutlineTextField(userSearch: "")
+        .padding(15)
 }
